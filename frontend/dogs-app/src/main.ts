2fa5e07394276fe75d7132961844a646e7f9614a
leoreblin <leoreblin@gmail.com>
2024-12-16 15:16:53 -0300

    refactor: removing vuetify and vue material

--- frontend/dogs-app/src/main.ts
+++ frontend/dogs-app/src/main.ts
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue';
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
 
 import router from './router';
-
-const vuetify = createVuetify();
 
 const app = createApp(App);
 
 app.use(router);
-app.use(vuetify);
 app.mount('#app');
